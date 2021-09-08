66a487608f651065ab5648336ac2cf1f2b04d386
Sid220 <74916637+Sid220@users.noreply.github.com>
2021-09-08 17:02:26 -0400

    Update uninstall.sh

--- 1/uninstall.sh
+++ 1/uninstall.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-jq 'del(.extensions[] | select(.name == "run in terminal"))' ../../prefs.json > tmp.$$.json && mv tmp.$$.json ../../prefs.json
+jq 'del(.extensions[] | select(.name == "run in terminal"))' prefs.json > tmp.$$.json && mv tmp.$$.json prefs.json
