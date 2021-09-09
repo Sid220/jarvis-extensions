46d5c3aaa686cc9147237486a23cd26b3e36d592
Sid220 <74916637+Sid220@users.noreply.github.com>
2021-09-09 16:25:13 -0400

    Update jarvis-main.js

--- 1/jarvis-main.js
+++ 1/jarvis-main.js
@@ -1,11 +1,22 @@
 function runInTerminal() {
-    cmd = byId("maininput").value;
+    var attr = new Array;
+    cmd = byId("maininput").value.toLowerCase().split(" ")[0];
+    for(i = 1; i < byId("maininput").value.toLowerCase().split(" ").length;i++) {
+        attr.push(byId("maininput").value.toLowerCase().split(" ")[i]);
+    }
     const { runInTerminal } = require('run-in-terminal');
     found = [{
         "a": "Running...",
         "b": "RUN IN TERMINAL"
     }]
+    if(byId("maininput").value.toLowerCase().split(" ").length > 1) {
+    runInTerminal(cmd, attr, {
+        cwd: process.cwd()
+    });
+}
+else {
     runInTerminal(cmd, null, {
         cwd: process.cwd()
     });
+}
 }
